1bbf694326ccfae2bd2b9d709c8cde9c48b5c3c3
e174596549 <ly6819@hotmail.com>
2017-03-28 11:10:27 +0800

    add something

---
+++ todo/src/components/todo.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="todo">
+  <h1 v-text="title"></h1>
+  <ul>
+    <li v-for="item in items" v-text="item.label"></li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data() {
+    return {
+      title: 'Welcome to Your TODO List',
+      items: [{
+        label: 'coding',
+        isFinished: false
+      }, {
+        label: 'walking',
+        isFinished: false
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
